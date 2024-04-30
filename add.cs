51aa11c7888c9fc18fe64473395aa7585bb6be60
diyastella15 <152716213+diyastella15@users.noreply.github.com>
2024-04-30 20:00:24 +0530

    Update add.cs

--- add.cs
+++ add.cs
@@ -1,1 +1,21 @@
-
+#include <stdio.h>
+int main() {
+ 
+    int number1, number2, diff;
+ 
+    printf("Enter two integers: ");
+    scanf("%d %d", &number1, &number2);
+ 
+    // calculate the diff
+    diff = number1 - number2;
+    printf("%d - %d = %d", number1, number2, diff);
+    return 0;
+}
+//function added in branch1
+int func_diff(int a, int b)
+{
+        // i am in main branch
+        // i am making this changes in
+        // one more change
+        return a-b;
+}
